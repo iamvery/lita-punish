495cda1ec9bb66cb91388387412a76056f83f42c
Jay Hayes <ur@iamvery.com>
2015-08-27 19:08:22 -0500

    Add super secret command to find out what room the bot sees

--- lib/lita/handlers/punish.rb
+++ lib/lita/handlers/punish.rb
@@ -9,11 +9,16 @@ module Lita
       ]
 
       route /^(?!TIL)/i, :punish
+      route /roome/, :roome, command: true
 
       def punish(response)
         if response.message.source.room == "#today-i-learned"
           response.reply SNARK.sample
         end
+      end
+
+      def roome(response)
+        response.reply_privately response.message.source.room
       end
 
       Lita.register_handler(self)
